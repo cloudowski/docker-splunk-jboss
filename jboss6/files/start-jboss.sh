062a5a2a533499284e6d47c6ab086222d818c3c4
Tomasz Cholewa <tch@linuxpolska.pl>
2014-06-05 10:50:16 +0200

    Fix for jboss6

--- jboss6/files/start-jboss.sh
+++ jboss6/files/start-jboss.sh
@@ -7,5 +7,9 @@ IPADDR=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/
 
 export JAVA_HOME=/usr/java/latest
 
+conf=`find /opt -name standalone-ha.xml`
+
+sed -i '/<\/extensions>/a<system-properties>\n<property name="org.apache.tomcat.util.ENABLE_MODELER" value="true"/>\n</system-properties>' $conf
+
 #/opt/jboss-eap-6.2/bin/standalone.sh -c standalone-ha.xml -Djboss.bind.address=$IPADDR -Djboss.bind.address.management=$IPADDR -Djboss.node.name=server-$IPADDR
 /opt/jboss-eap-6.2/bin/standalone.sh -c standalone-ha.xml -Djboss.bind.address=$IPADDR -Djboss.node.name=server-$IPADDR
